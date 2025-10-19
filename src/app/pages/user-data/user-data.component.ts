62215d4d8436aca8da66debbb86a923655f7fbb5
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2025-10-19 12:10:53 -0300

    create modal-dialog

--- src/app/pages/user-data/user-data.component.ts
+++ src/app/pages/user-data/user-data.component.ts
@@ -6,6 +6,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { ModalDialogComponent } from '../../shared/components/modal-dialog/modal-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-data',
@@ -16,14 +20,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatDividerModule,
+    MatIconModule,
   ],
   templateUrl: './user-data.component.html',
   styleUrl: './user-data.component.scss',
 })
 export class UserDataComponent {
-  
   private formBuilder = inject(FormBuilder);
   private userService = inject(UserService);
+  readonly dialog = inject(MatDialog);
 
   user = this.userService.getUser();
 
@@ -31,4 +37,24 @@ export class UserDataComponent {
     nome: [{ value: this.user?.nome || '', disabled: true }],
     email: [{ value: this.user?.email || '', disabled: true }],
   });
+
+  cadastrarEndereco() {
+    const dialogRef = this.dialog.open(ModalDialogComponent, {
+      data: { title: 'Adicionar Endereço' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  cadastrarTelefone() {
+    const dialogRef = this.dialog.open(ModalDialogComponent, {
+      data: { title: 'Adicionar Telefone' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
